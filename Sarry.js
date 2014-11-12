6e0ab6f1ce5ca277c854978e1631254e0c0c255a
Siddesh Bhalke <siddeshbhalke@gmail.com>
2014-11-12 13:22:20 +0530

    Update Sarry.js
    Updated

--- Sarry.js
+++ Sarry.js
@@ -1,7 +1,6 @@
-function pass(arg) {
+function SearchInArray(arg) {
         var array = ['open', 'opus', 'apple', 'app'];
         var result = [];
-
         for (var i = 0; i < array.length ; i++) {
           var patt = new RegExp(arg);
             if (patt.test(array[i])) {
@@ -11,3 +10,4 @@ function pass(arg) {
             return result;
         }
 
+To Call : SearchInArray('op') // result ['open','opus']
